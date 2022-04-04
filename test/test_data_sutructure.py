e1c60096a641c9fc1a3dd07f48916df4012be5f8
Hyoseok SEOL <95thomas811@gmail.com>
2022-04-04 15:09:09 +0900

    test vector3 to string

--- test/test_data_sutructure.py
+++ test/test_data_sutructure.py
@@ -1,9 +1,8 @@
-from decimal import DivisionByZero
 import pytest
 import numpy as np
 from data_structure.math import *
 
-class Test_Vector3():
+class TestVector3():
     def test_initialize(self):
         test_vec3 = Vector3()
         assert test_vec3.x == 0 and test_vec3.y == 0 and test_vec3.z == 0
@@ -17,12 +16,16 @@ class Test_Vector3():
         with pytest.raises(WrongInputException):
             test_nparray = np.array([1,2,3,4])
             Vector3.from_numpy(test_nparray)
+            test_list = [1,2,3]
     
     def test_vector3_to_numpy(self):
         test_nparray = np.array([4,5,6])
         test_vec3 = Vector3.from_numpy(test_nparray)
         assert np.allclose(test_vec3.to_numpy(), test_nparray)
 
+    def test_vector3_to_string(self):
+        test_vec3 = Vector3.from_numpy(np.array([1,2,3]))
+        assert str(test_vec3) == "Vector3(1, 2, 3)"
 
         
             
