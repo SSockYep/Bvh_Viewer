f2f01fa3f0b6ee0bf2d44262cb6fd0b5b731e7f5
Hyoseok SEOL <95thomas811@gmail.com>
2022-04-04 14:05:35 +0900

    Vector3 from numpy test

--- test/test_data_sutructure.py
+++ test/test_data_sutructure.py
@@ -5,3 +5,8 @@ class Test_Vector3():
     def test_initialize(self):
         test_vec3 = Vector3()
         return test_vec3.x == 0 and test_vec3.y == 0 and test_vec3.z == 0
+    
+    def test_vector3_from_numpy(self):
+        test_nparray = np.array([1,2,3])
+        test_vec3 = Vector3.from_numpy(test_nparray)
+        return test_vec3.x == 1 and test_vec3.y == 2 and test_vec3.z == 3
