cc9ba9acd1b172f864806bb36cef1c6f574e0cd4
Hyoseok SEOL <95thomas811@gmail.com>
2022-04-04 14:32:45 +0900

    test wrong numpy array input assertion

--- test/test_data_sutructure.py
+++ test/test_data_sutructure.py
@@ -1,12 +1,23 @@
+from decimal import DivisionByZero
+import pytest
 import numpy as np
-from data_structure.math import Vector3
+from data_structure.math import *
 
 class Test_Vector3():
     def test_initialize(self):
         test_vec3 = Vector3()
-        return test_vec3.x == 0 and test_vec3.y == 0 and test_vec3.z == 0
+        assert test_vec3.x == 0 and test_vec3.y == 0 and test_vec3.z == 0
     
     def test_vector3_from_numpy(self):
         test_nparray = np.array([1,2,3])
         test_vec3 = Vector3.from_numpy(test_nparray)
-        return test_vec3.x == 1 and test_vec3.y == 2 and test_vec3.z == 3
+        assert test_vec3.x == 1 and test_vec3.y == 2 and test_vec3.z == 3
+
+    def test_wrong_numpy_array(self):
+        with pytest.raises(WrongInputException):
+            test_nparray = np.array([1,2,3,4])
+            Vector3.from_numpy(test_nparray)
+
+
+        
+            
