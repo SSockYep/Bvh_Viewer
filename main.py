f51bbda5c4cfc9c26a66700b8059e2456ec8d73d
Hyoseok SEOL <95thomas811@gmail.com>
2022-04-04 14:02:57 +0900

    Vector3 initailize test pass

---
+++ main.py
@@ -0,0 +1,4 @@
+from data_structure import *
+
+if __name__ == '__main__':
+    pass
--- test/test_data_sutructure.py
+++ test/test_data_sutructure.py
@@ -1,7 +1,7 @@
 import numpy as np
-from data_structure import *
+from data_structure.math import Vector3
 
 class Test_Vector3():
-    def test_initialize():
+    def test_initialize(self):
         test_vec3 = Vector3()
         return test_vec3.x == 0 and test_vec3.y == 0 and test_vec3.z == 0
