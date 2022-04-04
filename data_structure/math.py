8236617ff101b4e408ce20e338a97e6b8ad4eb33
Hyoseok SEOL <95thomas811@gmail.com>
2022-04-04 14:37:59 +0900

    raise exception test pass

--- data_structure/math.py
+++ data_structure/math.py
@@ -7,8 +7,16 @@ class Vector3:
 
     @staticmethod
     def from_numpy(np_array):
+        if np_array.size != 3 or len(np_array.shape) != 1:
+            raise WrongInputException(np_array)
         new_vector3 = Vector3()
         new_vector3.x = np_array[0]
         new_vector3.y = np_array[1]
         new_vector3.z = np_array[2]
         return new_vector3
+
+class WrongInputException(Exception):
+    def __init__(self, inputs, message="WrongInput"):
+        self.inputs = inputs
+        self.message = message
+        super().__init__(self.message)
