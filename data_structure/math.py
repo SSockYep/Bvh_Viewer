c64815b24066918fce67a46a0dca110ed6129387
Hyoseok SEOL <95thomas811@gmail.com>
2022-04-04 14:09:51 +0900

    Vector3 from np test pass

--- data_structure/math.py
+++ data_structure/math.py
@@ -1,5 +1,14 @@
+import numpy as np
 class Vector3:
     def __init__(self):
         self.x = 0
         self.y = 0
         self.z = 0
+
+    @staticmethod
+    def from_numpy(np_array):
+        new_vector3 = Vector3()
+        new_vector3.x = np_array[0]
+        new_vector3.y = np_array[1]
+        new_vector3.z = np_array[2]
+        return new_vector3
