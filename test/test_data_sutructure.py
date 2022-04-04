6001136ba21a37b0f2b27e96773e114154925c81
Hyoseok SEOL <95thomas811@gmail.com>
2022-04-05 00:10:54 +0900

    Quaternion from matrix4x4 test

--- test/test_data_sutructure.py
+++ test/test_data_sutructure.py
@@ -68,8 +68,14 @@ class TestQuaternion:
         test_mat = Matrix4x4(test_nparr)
         assert quater.to_matrix() == test_mat
     
-    def test_from_euler(self):
-        pass
+    def test_from_matrix(self):
+        # matrix and quaternion for rotate pi/6, pi/4, pi/3 (euler xyz)
+        test_mat = np.array([[ 0.35355339, -0.5732233 ,  0.73919892, 0.],
+                             [ 0.61237244,  0.73919892,  0.28033009, 0.],
+                             [-0.70710678,  0.35355339,  0.61237244, 0.],
+                             [0., 0., 0., 1.]])
+        print(Quaternion.from_matrix(test_mat))
+        assert Quaternion.from_matrix(test_mat) == Quaternion(0.82236317, 0.02226003, 0.43967974, 0.36042341)
 
 class TestMatrix4x4:
     def test_matrix_init(self):
