8c438b6555ef33e9cc01b29ca5fde3d5151da278
Hyoseok Seol <95thomas811@gmail.com>
2022-04-05 23:03:24 +0900

    node set offset on init test pass

--- utility/bvh_tree.py
+++ utility/bvh_tree.py
@@ -1,6 +1,8 @@
 import numpy as np
+from data_structure.math import *
 
 class Node:
-    def __init__(self):
+    def __init__(self, offset=Vector3(0,0,0)):
         self.parent = None
         self.children = None
+        self.offset = offset
