42c81979b44c45c5cca3cd30795eb347434da2ae
설효석 <2014004075@hanyang.ac.kr>
2022-04-09 21:47:06 +0900

    Vector3: calculation test pass

--- data_structure/math.py
+++ data_structure/math.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+
 class Vector3:
-    def __init__(self, x:np.float32=0., y:np.float32=0., z:np.float32=0.):
+    def __init__(self, x: np.float32 = 0.0, y: np.float32 = 0.0, z: np.float32 = 0.0):
         self.x = x
         self.y = y
         self.z = z
@@ -10,8 +11,26 @@ class Vector3:
         return "Vector3({}, {}, {})".format(self.x, self.y, self.z)
 
     def __eq__(self, other):
-        return np.isclose(self.x, other.x) and \
-            np.isclose(self.y, other.y) and np.isclose(self.z, other.z)
+        return (
+            np.isclose(self.x, other.x)
+            and np.isclose(self.y, other.y)
+            and np.isclose(self.z, other.z)
+        )
+
+    def __nq__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, other):  # self + other: Vector addition
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):  # self - other: Vector subtraction
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __truediv__(self, other):  # self / othher: Vector divide by scalar
+        return Vector3(self.x / other, self.y / other, self.z / other)
+
+    def __matmul__(self, other):  # self @ other: Vector dot product
+        return self.x * other.x + self.y * other.y + self.z * other.z
 
     @classmethod
     def from_numpy(cls, np_array):
@@ -22,40 +41,54 @@ class Vector3:
         return cls(np_array[0], np_array[1], np_array[2])
 
     def to_numpy(self, dtype=np.float32):
-        return np.array([self.x, self.y, self.z],dtype=dtype)
+        return np.array([self.x, self.y, self.z], dtype=dtype)
+
+    def magnitude(self):
+        return np.sqrt(self @ self)
 
     def copy(self):
         return Vector3(self.x, self.y, self.z)
 
 
 class Quaternion:
-    def __init__(self, w:np.float32=1, x:np.float32=0, y:np.float32=0, z:np.float32=0):
+    def __init__(
+        self, w: np.float32 = 1, x: np.float32 = 0, y: np.float32 = 0, z: np.float32 = 0
+    ):
         self.w = w
         self.x = x
         self.y = y
         self.z = z
 
     def __eq__(self, other):
-        return np.isclose(self.w, other.w) and np.isclose(self.x, other.x) \
-               and np.isclose(self.y, other.y) and np.isclose(self.z, other.z)
+        return (
+            np.isclose(self.w, other.w)
+            and np.isclose(self.x, other.x)
+            and np.isclose(self.y, other.y)
+            and np.isclose(self.z, other.z)
+        )
 
     def __str__(self):
         return "Quaternion({}, {}, {}, {})".format(self.w, self.x, self.y, self.z)
-    
+
     def __mul__(self, other):
         if not isinstance(other, Quaternion):
             assert TypeError(other)
         a, b, c, d = self.w, self.x, self.y, self.z
         e, f, g, h = other.w, other.x, other.y, other.z
-        res = Quaternion((a*e - b*f - c*g - d*h), (a*f + b*e + c*h - d*g),
-                          (a*g - b*h + c*e + d*f), (a*h + b*g - c*f + d*e))
+        res = Quaternion(
+            (a * e - b * f - c * g - d * h),
+            (a * f + b * e + c * h - d * g),
+            (a * g - b * h + c * e + d * f),
+            (a * h + b * g - c * f + d * e),
+        )
         return res
 
     def conjugate(self):
         return Quaternion(self.w, -self.x, -self.y, -self.z)
 
-class Matrix4x4: 
-    def __init__(self, np_array=np.eye(4,4)):
+
+class Matrix4x4:
+    def __init__(self, np_array=np.eye(4, 4)):
         if type(np_array) != np.ndarray:
             raise ValueError
         if np_array.shape != (4, 4):
@@ -69,46 +102,65 @@ class Matrix4x4:
         row = pair[0]
         col = pair[1]
         return self._mat[row, col]
-    
+
     def __matmul__(self, other):
         if isinstance(other, Matrix4x4):
             return Matrix4x4(self._mat @ other._mat)
         elif isinstance(other, Vector3):
             np_vec = other.to_numpy()
             np_vec = np.append(np_vec, 1)
-            return Vector3.from_numpy((self._mat@np_vec)[:3])
-    
+            return Vector3.from_numpy((self._mat @ np_vec)[:3])
+
     @classmethod
     def from_euler(cls, seq, *angles):
         def rotate(axis, angle):
-            if axis == 'x' or axis == 'X':
-                return Matrix4x4(np.array([[1, 0, 0, 0],
-                                           [0, np.cos(angle), -np.sin(angle), 0],
-                                           [0, np.sin(angle), np.cos(angle), 0],
-                                           [0, 0, 0, 1]]))
-            elif axis == 'y' or axis == 'Y':
-                return Matrix4x4(np.array([[np.cos(angle), 0, np.sin(angle), 0],
-                                           [0, 1, 0, 0],
-                                           [-np.sin(angle), 0, np.cos(angle), 0],
-                                           [0, 0, 0, 1]]))
-            elif axis == 'z' or axis == 'Z':
-                return Matrix4x4(np.array([[np.cos(angle), -np.sin(angle), 0, 0],
-                                           [np.sin(angle), np.cos(angle), 0, 0],
-                                           [0, 0, 1, 0],
-                                           [0, 0, 0, 1]]))
+            if axis == "x" or axis == "X":
+                return Matrix4x4(
+                    np.array(
+                        [
+                            [1, 0, 0, 0],
+                            [0, np.cos(angle), -np.sin(angle), 0],
+                            [0, np.sin(angle), np.cos(angle), 0],
+                            [0, 0, 0, 1],
+                        ]
+                    )
+                )
+            elif axis == "y" or axis == "Y":
+                return Matrix4x4(
+                    np.array(
+                        [
+                            [np.cos(angle), 0, np.sin(angle), 0],
+                            [0, 1, 0, 0],
+                            [-np.sin(angle), 0, np.cos(angle), 0],
+                            [0, 0, 0, 1],
+                        ]
+                    )
+                )
+            elif axis == "z" or axis == "Z":
+                return Matrix4x4(
+                    np.array(
+                        [
+                            [np.cos(angle), -np.sin(angle), 0, 0],
+                            [np.sin(angle), np.cos(angle), 0, 0],
+                            [0, 0, 1, 0],
+                            [0, 0, 0, 1],
+                        ]
+                    )
+                )
             else:
                 raise ValueError
+
         if type(seq) != str:
             raise ValueError
         if len(seq) != 3:
             raise ValueError
-        if seq[0]==seq[1] or seq[1] == seq[2]:
+        if seq[0] == seq[1] or seq[1] == seq[2]:
             raise ValueError
-        if seq[0] not in 'xyz' or seq[1] not in 'xyz' or seq[2] not in 'xyz':
+        if seq[0] not in "xyz" or seq[1] not in "xyz" or seq[2] not in "xyz":
             raise ValueError
         if len(angles) != 3:
             raise ValueError
         mat0 = rotate(seq[0], angles[0])
         mat1 = rotate(seq[1], angles[1])
         mat2 = rotate(seq[2], angles[2])
-        return cls((mat0@mat1@mat2)._mat)
+        return cls((mat0 @ mat1 @ mat2)._mat)
